69e2679205f0246fd3f7d1e5b4f8b7b4eac97d63
Naveen-759 <naveenku759@gmail.com>
2024-09-24 12:11:11 +0530

    CORS error resolved

--- Backend/Server.js
+++ Backend/Server.js
@@ -9,7 +9,7 @@ dbConnection();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
   methods: "GET , POST , PUT , DELETE , PATCH",
   credentials: true,
 };
